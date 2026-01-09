f90161fcd17459ae66c6c57db11916ffd4b1a490
gadivenkatapraveen <gadivenkatapraveen@gmail.com>
2026-01-09 11:00:39 +0530

    Assignment 1 - Data types

---
+++ typescript-training/language-basics/first-ts-program.ts
@@ -0,0 +1,3 @@
+//Hello world program in typescript
+
+console.log("Hello, World!");
---
+++ typescript-training/language-basics/variable-declaration.ts
@@ -0,0 +1,11 @@
+// variable decalartion will vary based on 4 paramters
+
+//1. Inistaialization
+//2. reassignment
+//3. redeclaration
+//4. scope (block scope {})
+
+//1. Inistaialization
+let a :number = 10; //initialized
+//
+console.log(a);
